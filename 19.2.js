5079e214a1cf13107d9dcdb28f8f806e59ae578c
HasanOmar1 <r3domar1@gmail.com>
2023-10-02 13:16:49 +0300

    added

---
+++ 19.2.js
@@ -0,0 +1,52 @@
+
+const users = [
+    {
+    id: 167464,
+    firstName: "Jimmy",
+    lastName: "Arnold",
+    email: "jimmya@gmail.com",
+    },
+    {
+    id: 578447,
+    firstName: "Martha",
+    lastName: "Goldman",
+    email: "gold@hotmail.com",
+    },
+    {
+    id: 864578,
+    firstName: "Tim",
+    lastName: "Burton",
+    email: "timmy.hotmail.com",
+    },
+    ];
+
+
+    //1. Loop over the array with the forEach method - V
+    // dynamically create an ordered list - V
+    //  of the first and last names of the objects.
+
+    const ol = document.createElement("ol")
+    users.forEach((user) => {
+        const li = document.createElement("li")
+        li.innerText = user.firstName + " " + user.lastName;
+
+        //Create a custom data attribute called “data-id” and attach the id value to each li.
+        //create new attribute
+        const dataId = document.createAttribute("data-id")
+        dataId.value = Math.floor(Math.random() * 1000)
+        li.setAttribute("data-id",Math.floor(Math.random() * 1000))
+        ol.append(li)
+    })
+
+    const body = document.body;
+    body.appendChild(ol)
+
+//remove the numbers from the list
+ol.style.listStyleType = "none"
+
+
+
+
+
+
+
