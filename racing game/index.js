464ff04ccbefce27e1a600095634d1a9bf3922c2
HasanOmar1 <r3domar1@gmail.com>
2023-10-05 10:08:41 +0300

    added

--- racing game/index.js
+++ racing game/index.js
@@ -1,27 +1,22 @@
-
-    function moveForward(player){
-    const car = document.querySelector(`#player${player}-race .active`)
-    if(car.nextElementSibling ){
+function moveForward(playerId){
+    const car = document.querySelector(`#player${playerId}-race .active`)
+    if(car.nextElementSibling){
         car.nextElementSibling.classList.add('active')
-        car.classList.remove('active')   
+        car.classList.remove('active')
     }
     else{
-        alert(`Player ${player} wins! Play again?`)
-        window.location.reload();
+        alert(`The winner is Player ${playerId} ! Play again?`)
+        window.location.reload()
     }
+}
+
+function controls(e){
+    if(e.key === 'e'){
+        moveForward(1)
     }
-
-    function controls(e){
-        if(e.key === 'e'){
-            moveForward(1)
-        }
-        else if(e.key === 'c'){
-            moveForward(2)
-            
-        }
-    
+    else if(e.key === 'c'){
+        moveForward(2)
     }
-    document.addEventListener('keyup', controls);
+}
 
-
-
+document.addEventListener('keyup' , controls)
