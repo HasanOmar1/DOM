604f35a70a4a0dacb1cfeeefc9178ae559a4b4fa
HasanOmar1 <r3domar1@gmail.com>
2023-11-13 11:16:25 +0200

    added

--- 23.2super-powers.js
+++ 23.2super-powers.js
@@ -1,10 +1,11 @@
 const storm = {
     // add code here
     superPower: `flying`,
-    func: function printSuperPower() {
-    console.log("my superpower is " + this.superPower)
-    }
+    func: printSuperPower
     }
 
+    function printSuperPower() {
+    console.log("my superpower is " + this.superPower)
+    }
 
     storm.func()
