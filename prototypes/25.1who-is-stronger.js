f9dda748bb31b5ce2a26233c3fa4e099f22ad901
HasanOmar1 <r3domar1@gmail.com>
2023-11-15 13:59:41 +0200

    added

--- prototypes/25.1who-is-stronger.js
+++ prototypes/25.1who-is-stronger.js
@@ -24,4 +24,15 @@ console.log(whoIsStronger(stronger))
 // 2. Fix the code so that invoking the whoIsStronger function
 // will print "I am stronger".
 
-//getStrength() inside whoIsStronger function is not defined
+
+// <!-- The problem with the code is that when you invoke the
+//      whoIsStronger function with hero.getStrength, 
+//      the this context inside the getStrength method of the
+//       hero object is lost. This happens because getStrength
+//        is called as a standalone function, and within the function,
+//         this does not refer to the hero object. As a result,
+//          the comparison in whoIsStronger is not working as expected.
+
+// To fix the code and make it print "I am stronger,"
+//  you can use the bind method to explicitly bind the
+//   this context to the hero object when calling getStrength.-->
