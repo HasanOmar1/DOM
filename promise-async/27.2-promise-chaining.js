59e4d3ed467939ceefb860ebf355a172a4060c6f
HasanOmar1 <r3domar1@gmail.com>
2023-11-16 16:05:30 +0200

    added

--- promise-async/27.2-promise-chaining.js
+++ promise-async/27.2-promise-chaining.js
@@ -1,22 +1,30 @@
 
-const words = [`Kola` , `fifa` , `asd`, `SpongeBob` , `noIce`]
+const words = [`Kola`, 5,`fifa` , `asd`, `SpongeBob` , `noIce`]
 
+    const makeAllCaps = () => {
+        return new Promise((resolve , reject) =>{
+            const upperCase = words.map(u => u.toUpperCase())
+            console.log(upperCase)
+        })
+    }
+    
+    const sortWords = () => {
+        return new Promise((resolve , reject) => {
+            const sorted = words.map(u => u.toUpperCase()).sort()
+            resolve(sorted)
+            for(let strings of words){
+                if(sorted.includes(typeof strings !== string)){
+                        return reject()
+        
+                    }
+            }
+            
 
-
-const makeAllCaps = () => {
-    return new Promise((resolve , reject) =>{
-        const upperCase = words.map(u => u.toUpperCase())
-        console.log(upperCase)
-    })
+        })
 }
 
-const sortWords = () => {
-    return new Promise((resolve , reject) => {
-        const sorted = words.map(u => u.toUpperCase()).sort()
-        console.log(sorted)
 
-    })
-}
-
-console.log(makeAllCaps())
-console.log(sortWords())
+// console.log(makeAllCaps())
+sortWords()
+.then((data) =>console.log(data) )
+.catch(() => console.log('not string!'))
