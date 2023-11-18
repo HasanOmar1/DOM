1f46842b41c28f8594527467cf01b9f3044545a7
HasanOmar1 <r3domar1@gmail.com>
2023-11-18 21:05:31 +0200

    added

--- promise-async/27.2-promise-chaining.js
+++ promise-async/27.2-promise-chaining.js
@@ -1,30 +1,26 @@
 
-const words = [`Kola`, 5,`fifa` , `asd`, `SpongeBob` , `noIce`]
-
-    // const makeAllCaps = () => {
-    //     return new Promise((resolve , reject) =>{
-    //         const upperCase = words.map(u => u.toUpperCase())
-    //         console.log(upperCase)
-    //     })
-    // }
-    
-    const sortWords = () => {
-        return new Promise((resolve , reject) => {
-            const sorted = words.map(u => u.toUpperCase()).sort()
-            resolve(sorted)
-            for(let strings of words){
-                if(sorted.includes(typeof strings !== string)){
-                        return reject()
-        
-                    }
+const  makeAllCaps = (arr) => {
+    const array = []
+    return new Promise((resolve , reject) => {
+        for(let word of arr){
+            if(typeof word === `string`){
+                array.push(word.toUpperCase())
             }
-            
+            else{
+                reject('Only strings allowed!')
+            }
+        }
+        resolve(array)
+    })
 
-        })
 }
 
+const sortArr = (arr) => {
+    return arr.sort()
+}
 
-// console.log(makeAllCaps())
-sortWords()
-.then((data) =>console.log(data) )
-.catch(() => console.log('not string!'))
+const newArr = [`Hello ` , `world` , `asd`, 19]
+
+makeAllCaps(newArr)
+.then(upperArray => console.log(sortArr(upperArray)))
+.catch(error => console.log(error))
---
+++ promise-async/29.1-async-await.js
@@ -0,0 +1,8 @@
+const getIDs = () =>
+new Promise((resolve, reject) => {
+setTimeout(() => {
+resolve([532, 543, 753, 1, 5]);
+}, 1500);
+});
+
+getIDs().then((arr) => console.log(arr))
