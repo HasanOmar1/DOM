bbe6e4c6529c929375b46acab469a412152620af
HasanOmar1 <r3domar1@gmail.com>
2023-11-19 15:34:55 +0200

    added

--- promise-async/27.2-promise-chaining.js
+++ promise-async/27.2-promise-chaining.js
@@ -5,12 +5,12 @@ const  makeAllCaps = (arr) => {
         for(let word of arr){
             if(typeof word === `string`){
                 array.push(word.toUpperCase())
+                resolve(array)
             }
             else{
                 reject('Only strings allowed!')
             }
         }
-        resolve(array)
     })
 
 }
@@ -19,7 +19,7 @@ const sortArr = (arr) => {
     return arr.sort()
 }
 
-const newArr = [`Hello ` , `world` , `asd`, 19]
+const newArr = [`Hello ` , `world` , `asd`]
 
 makeAllCaps(newArr)
 .then(upperArray => console.log(sortArr(upperArray)))
